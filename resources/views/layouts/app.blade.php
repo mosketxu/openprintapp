1020ac3f921957daa59fd3b804bfb348288e4223
Alexander Arregui <mosketxu@gmail.com>
2025-06-30 11:03:54 +0200

    Vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,13 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']). --}}
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
         <!-- Styles -->
         @livewireStyles
