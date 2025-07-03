9db8f71aa416a0b26a65c2f8a88cd4911039add0
Alexander Arregui <mosketxu@gmail.com>
2025-07-03 12:18:45 +0200

    segunfdo intento

--- resources/views/campaign/etiquetashtml.blade.php
+++ resources/views/campaign/etiquetashtml.blade.php
@@ -23,7 +23,7 @@
                             <tr style="background-color: #139cdc;">
                                 <th style="text-align: right;" width="25%">
                                     {{-- <img src="{{asset('img/logo.png')}}" width="50px"> --}}
-                                    <img src="{{public_path('storage/imagenes/logo.png')}}" width="50px">
+                                    <img src="{{public_path('storage/img/logo.png')}}" width="50px">
                                 </th>
                                 <th style="color:#ffffff;text-align:center;"  width="50%">
                                     Cliente: {{$campaign->entidad->entidad}} <br>
